4447165484c7a45a84d326230681e6184bac06de
kingnetMac <kingnetmac@kingnetMacdeiMac.local>
2016-01-14 14:05:29 +0800

    sepc

---
+++ thirdLib/WZMarqueeView.h
@@ -0,0 +1,16 @@
+//
+//  WZMarqueeView.h
+//  WZMarqueeViewDemo
+//
+//  Created by wangzz on 14-3-2.
+//  Copyright (c) 2014年 foogry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZMarqueeView : UIView
+
+@property (nonatomic, strong) UILabel *lable;
+@property (nonatomic) NSTimeInterval duration;
+
+@end
